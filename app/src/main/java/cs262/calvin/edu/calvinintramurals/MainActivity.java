868ec4ac3d809257d922c359f1076d2708b4c715
bdv9 <bdv9@students.calvin.edu>
2015-09-27 12:25:01 -0400

    carousel

--- app/src/main/java/cs262/calvin/edu/calvinintramurals/MainActivity.java
+++ app/src/main/java/cs262/calvin/edu/calvinintramurals/MainActivity.java
@@ -1,19 +1,55 @@
 package cs262.calvin.edu.calvinintramurals;
 
-import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+    public final static int PAGES = 5;
+    // You can choose a bigger number for LOOPS, but you know, nobody will fling
+    // more than 1000 times just in order to test your "infinite" ViewPager :D
+    public final static int LOOPS = 1000;
+    public final static int FIRST_PAGE = PAGES * LOOPS / 2;
+    public final static float BIG_SCALE = 1.0f;
+    public final static float SMALL_SCALE = 0.7f;
+    public final static float DIFF_SCALE = BIG_SCALE - SMALL_SCALE;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private int[] musicCover = { R.drawable.cover1, R.drawable.cover2,
+            R.drawable.cover3, R.drawable.cover4, R.drawable.cover5};
+
+    public MyPagerAdapter adapter;
+    public ViewPager pager;
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pager = (ViewPager) findViewById(R.id.myviewpager);
+
+        adapter = new MyPagerAdapter(this, this.getSupportFragmentManager());
+        pager.setAdapter(adapter);
+        pager.setOnPageChangeListener(adapter);
+
+
+        // Set current item to the middle page so we can fling to both
+        // directions left and right
+        pager.setCurrentItem(FIRST_PAGE);
+
+        // Necessary or the pager will only have one extra page to show
+        // make this at least however many pages you can see
+        pager.setOffscreenPageLimit(3);
+
+        // Set margin for pages as a negative number, so a part of next and
+        // previous pages will be showed
+        pager.setPageMargin(-600);
+
+
+
     }
 
     @Override
@@ -37,7 +73,7 @@ public class MainActivity extends Activity {
                 return super.onOptionsItemSelected(item);
         }
     }
-
+    // Had to add these definitions
     private void openSearch(){
         startActivity(new Intent(SearchManager.INTENT_ACTION_GLOBAL_SEARCH));
     }
---
+++ app/src/main/java/cs262/calvin/edu/calvinintramurals/MyFragment.java
@@ -0,0 +1,59 @@
+package cs262.calvin.edu.calvinintramurals;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MyFragment extends Fragment {
+
+    public static Fragment newInstance(MainActivity context, int pos,
+                                       float scale)
+    {
+        Bundle b = new Bundle();
+        b.putInt("pos", pos);
+        b.putFloat("scale", scale);
+        return Fragment.instantiate(context, MyFragment.class.getName(), b);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (container == null) {
+            return null;
+        }
+
+        LinearLayout l = (LinearLayout)
+                inflater.inflate(R.layout.mf, container, false);
+
+        int pos = this.getArguments().getInt("pos");
+        TextView tv = (TextView) l.findViewById(R.id.text);
+
+        ImageView iv = (ImageView) l.findViewById(R.id.content);
+        if (pos == 0) {
+            iv.setImageResource(R.drawable.cover3);
+            tv.setText("Volleyball");}
+        else if (pos == 1) {
+            iv.setImageResource(R.drawable.cover4);
+            tv.setText("Ping Pong");}
+        else if (pos == 2) {
+            iv.setImageResource(R.drawable.cover5);
+            tv.setText("Football");}
+        else if (pos == 3) {
+            iv.setImageResource(R.drawable.cover1);
+            tv.setText("Soccer");}
+        else if (pos == 4) {
+            iv.setImageResource(R.drawable.cover2);
+        tv.setText("BasketBall");}
+
+        MyLinearLayout root = (MyLinearLayout) l.findViewById(R.id.root);
+        float scale = this.getArguments().getFloat("scale");
+        root.setScaleBoth(scale);
+
+        return l;
+    }
+}
